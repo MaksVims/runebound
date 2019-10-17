59efec25419d43c11939c86b28a07bec038a0050
Maksim_Smetanin <maks.s923@mail.ru>
2019-10-17 23:56:04 +0500

    test eventEmmiter -begin

---
+++ src/Game/GameCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <article class="game-card">
+    <img :src="src" :alt="card.title" class="game-card-img" />
+
+    <template v-if="active">
+      <div class="card-popup">
+        
+      </div>
+    </template>
+
+  </article>
+</template>
+
+<script>
+export default {
+  name: "GameCard",
+  props: {
+    card: {
+      type: Object,
+      required: true
+    },
+    src: String
+  },
+
+  data() {
+    return {
+      active: false
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.game-card-img {
+  width: 150px;
+  height: 200px;
+  cursor: pointer;
+  transition: transform .3s linear;
+
+  &:hover {
+    transform: scale(1.5);
+  }
+}
+</style>
--- src/Game/GlobalEvent.vue
+++ src/Game/GlobalEvent.vue
@@ -2,34 +2,36 @@
   <article class="game-globalEvent">
     <p>Глобальное событие</p>
     <div class="game-globalEvent--content">
-      <img
-      :src="`/img/content/globalEvent/${event.type}_${event.id}.png`"
-      :alt="event.title"
-      v-for='event of events'
-      :key="event.id"
-      class="card-event"
-      >
+      <GameCard
+        v-for="event of events"
+        :key="event.id"
+        :card="event"
+        :src="`/img/content/globalEvent/${event.type}_${event.id}.png`"
+        class="card-event"
+      />
     </div>
   </article>
 </template>
 
 <script>
+import GameCard from "./GameCard";
 
 export default {
+  components: {
+    GameCard
+  },
+
   computed: {
     events() {
       return this.$store.getters.getGame.globalEvent;
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-.game-globalEvent {
+.game-globalEvent,
+.game-globalEvent--content {
   display: flex;
-}
-.card-event {
-  width: 150px;
-  height: 200px;
 }
 </style>
